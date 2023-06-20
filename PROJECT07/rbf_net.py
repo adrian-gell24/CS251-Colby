47451c404e1850ea53807caca4cb94b5e5037f39
Adrian Gellert <adrian.gellert@colby.edu>
2023-06-19 22:46:37 -0400

    PROJ7

---
+++ PROJECT07/rbf_net.py
@@ -0,0 +1,274 @@
+'''rbf_net.py
+Radial Basis Function Neural Network
+Adrian Gellert
+CS 251: Data Analysis Visualization
+Spring 2023
+'''
+import numpy as np
+import kmeans
+
+class RBF_Net:
+    def __init__(self, num_hidden_units, num_classes):
+        '''RBF network constructor
+
+        Parameters:
+        -----------
+        num_hidden_units: int. Number of hidden units in network. NOTE: does NOT include bias unit
+        num_classes: int. Number of output units in network. Equals number of possible classes in
+            dataset
+
+        TODO:
+        - Define number of hidden units as an instance variable called `k` (as in k clusters)
+            (You can think of each hidden unit as being positioned at a cluster center)
+        - Define number of classes (number of output units in network) as an instance variable
+        '''
+        # prototypes: Hidden unit prototypes (i.e. center)
+        #   shape=(num_hidden_units, num_features)
+        self.prototypes = None
+        # sigmas: Hidden unit sigmas: controls how active each hidden unit becomes to inputs that
+        # are similar to the unit's prototype (i.e. center).
+        #   shape=(num_hidden_units,)
+        #   Larger sigma -> hidden unit becomes active to dissimilar inputs
+        #   Smaller sigma -> hidden unit only becomes active to similar inputs
+        self.sigmas = None
+        # wts: Weights connecting hidden and output layer neurons.
+        #   shape=(num_hidden_units+1, num_classes)
+        #   The reason for the +1 is to account for the bias (a hidden unit whose activation is always
+        #   set to 1).
+        self.wts = None
+        self.k = num_hidden_units
+        self.num_class = num_classes
+
+    def get_prototypes(self):
+        '''Returns the hidden layer prototypes (centers)
+
+        (Should not require any changes)
+
+        Returns:
+        -----------
+        ndarray. shape=(k, num_features).
+        '''
+        return self.prototypes
+
+    def get_num_hidden_units(self):
+        '''Returns the number of hidden layer prototypes (centers/"hidden units").
+
+        Returns:
+        -----------
+        int. Number of hidden units.
+        '''
+        return self.k
+
+    def get_num_output_units(self):
+        '''Returns the number of output layer units.
+
+        Returns:
+        -----------
+        int. Number of output units
+        '''
+        return self.num_class
+
+    def avg_cluster_dist(self, data, centroids, cluster_assignments, kmeans_obj):
+        '''Compute the average distance between each cluster center and data points that are
+        assigned to it.
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_features). Data to learn / train on.
+        centroids: ndarray. shape=(k, num_features). Centroids returned from K-means.
+        cluster_assignments: ndarray. shape=(num_samps,). Data sample-to-cluster-number assignment from K-means.
+        kmeans_obj: KMeans. Object created when performing K-means.
+
+        Returns:
+        -----------
+        ndarray. shape=(k,). Average distance within each of the `k` clusters.
+
+        Hint: A certain method in `kmeans_obj` could be very helpful here!
+        '''
+        averages = np.zeros((self.k,))
+        for i in range(self.k):
+            cluster = data[cluster_assignments == i]
+            dist = kmeans_obj.dist_pt_to_centroids(cluster, centroids[i])
+            averages[i] = np.mean(dist)
+        return averages
+
+    def initialize(self, data):
+        '''Initialize hidden unit centers using K-means clustering and initialize sigmas using the
+        average distance within each cluster
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_features). Data to learn / train on.
+
+        TODO:
+        - Determine `self.prototypes` (see constructor for shape). Prototypes are the centroids
+        returned by K-means. It is recommended to use the 'batch' version of K-means to reduce the
+        chance of getting poor initial centroids.
+            - To increase the chance that you pick good centroids, set the parameter controlling the
+            number of iterations > 1 (e.g. 5)
+        - Determine self.sigmas as the average distance between each cluster center and data points
+        that are assigned to it. Hint: You implemented a method to do this!
+        '''
+        kmeans_obj = kmeans.KMeans(data=data)
+        
+        kmeans_obj.cluster_batch(k=self.k, n_iter=5, verbose=False)
+        self.prototypes = kmeans_obj.get_centroids()
+        cluster_labels = kmeans_obj.get_data_centroid_labels()
+        
+        self.sigmas = self.avg_cluster_dist(data=data, centroids=self.prototypes,
+                                            cluster_assignments=cluster_labels, 
+                                            kmeans_obj=kmeans_obj)
+
+    def linear_regression(self, A, y):
+        '''Performs linear regression
+        CS251: Adapt your SciPy lstsq code from the linear regression project.
+        CS252: Adapt your QR-based linear regression solver
+
+        Parameters:
+        -----------
+        A: ndarray. shape=(num_data_samps, num_features).
+            Data matrix for independent variables.
+        y: ndarray. shape=(num_data_samps, 1).
+            Data column for dependent variable.
+
+        Returns
+        -----------
+        c: ndarray. shape=(num_features+1,)
+            Linear regression slope coefficients for each independent var AND the intercept term
+
+        NOTE: Remember to handle the intercept ("homogenous coordinate")
+        '''
+        ones = np.ones((A.shape[0],1))
+        Ahat = np.hstack((A, ones))
+        c, _, _, _ = np.linalg.lstsq(Ahat, y, rcond=None)
+        return c
+
+    def hidden_act(self, data):
+        '''Compute the activation of the hidden layer units
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_features). Data to learn / train on.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_samps, k).
+            Activation of each unit in the hidden layer to each of the data samples.
+            Do NOT include the bias unit activation.
+            See notebook for refresher on the activation equation
+        '''
+        # Compute distances between data and prototypes
+        hidden = np.zeros((data.shape[0], self.k))
+        
+        for i in range(data.shape[0]):
+            distance = np.sum(np.square(data[i] - self.prototypes), axis= 1)
+            sd = 2 * self.sigmas**2 + 1e-8
+            hidden[i] = np.exp(-distance/sd)
+        
+        return hidden
+
+    def output_act(self, hidden_acts):
+        '''Compute the activation of the output layer units
+
+        Parameters:
+        -----------
+        hidden_acts: ndarray. shape=(num_samps, k).
+            Activation of the hidden units to each of the data samples.
+            Does NOT include the bias unit activation.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_samps, num_output_units).
+            Activation of each unit in the output layer to each of the data samples.
+
+        NOTE:
+        - Assumes that learning has already taken place
+        - Can be done without any for loops.
+        - Don't forget about the bias unit!
+        '''
+        bias = np.ones((hidden_acts.shape[0],))
+        hidden_acts = np.vstack((hidden_acts.T, bias))
+        return hidden_acts.T @ self.wts
+
+    def train(self, data, y):
+        '''Train the radial basis function network
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_features). Data to learn / train on.
+        y: ndarray. shape=(num_samps,). Corresponding class of each data sample.
+
+        Goal: Set the weights between the hidden and output layer weights (self.wts) using
+        linear regression. The regression is between the hidden layer activation (to the data) and
+        the correct classes of each training sample. To solve for the weights going FROM all of the
+        hidden units TO output unit c, recode the class vector `y` to 1s and 0s:
+            1 if the class of a data sample in `y` is c
+            0 if the class of a data sample in `y` is not c
+
+        Notes:
+        - Remember to initialize the network (set hidden unit prototypes and sigmas based on data).
+        - Pay attention to the shape of self.wts in the constructor above. Yours needs to match.
+        - The linear regression method handles the bias unit.
+        '''
+        # Initialize network
+        self.initialize(data=data) # set self.prototypes, self.sigmas
+        
+        # Recode the class vector `y` to 1s and 0s
+        classifiers = np.unique(y)
+        y_binary = np.zeros((data.shape[0], len(classifiers)),dtype=int)
+        for i in range(data.shape[0]):
+            y_binary[i, int(y[i])] += 1
+        
+        hidden = self.hidden_act(data)
+        self.wts = self.linear_regression(A=hidden, y=y_binary)
+
+    def predict(self, data):
+        '''Classify each sample in `data`
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_features). Data to predict classes for.
+            Need not be the data used to train the network
+
+        Returns:
+        -----------
+        ndarray of nonnegative ints. shape=(num_samps,). Predicted class of each data sample.
+
+        TODO:
+        - Pass the data thru the network (input layer -> hidden layer -> output layer).
+        - For each data sample, the assigned class is the index of the output unit that produced the
+        largest activation.
+        '''        
+        # Compute hidden layer activations
+        hidden = self.hidden_act(data=data)
+        
+        # Compute output layer activations
+        output = self.output_act(hidden_acts=hidden)
+        
+        prediction = np.zeros((data.shape[0],))
+        for i in range(data.shape[0]):
+            prediction[i] = np.argmax(output[i])
+        
+        return prediction
+        
+
+    def accuracy(self, y, y_pred):
+        '''Computes accuracy based on percent correct: Proportion of predicted class labels `y_pred`
+        that match the true values `y`.
+
+        Parameters:
+        -----------
+        y: ndarray. shape=(num_data_sams,)
+            Ground-truth, known class labels for each data sample
+        y_pred: ndarray. shape=(num_data_sams,)
+            Predicted class labels by the model for each data sample
+
+        Returns:
+        -----------
+        float. Between 0 and 1. Proportion correct classification.
+
+        NOTE: Can be done without any loops
+        '''
+        proportion_equal = np.sum(y_pred == y) / len(y)
+        
+        return proportion_equal
